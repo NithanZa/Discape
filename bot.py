900a87da50ffd58f6f3a33fd8c3bcc1b0eb4bd31
NithanZa <jaroonekp@gmail.com>
2024-02-06 21:43:26 +0700

    initial bot setup

--- bot.py
+++ bot.py
@@ -1,14 +1,31 @@
 from os import getenv
 import discord
+from yaml import safe_load
+from sys import exit
+from dotenv import load_dotenv
 
-bot = discord.Bot()
+load_dotenv()
+TOKEN = getenv("BOT_TOKEN")
+
+with open('config.yml', 'r') as file:
+    CONFIG = safe_load(file)
+    if CONFIG is None:
+        print("config.yml not found, exiting program")
+        exit()
+
+DEBUG_MODE = CONFIG['debug_mode']
+
+if DEBUG_MODE:
+    DEBUG_GUILD_IDS = CONFIG['debug_guild_ids']
+    bot = discord.Bot(debug_guilds=DEBUG_GUILD_IDS)
+else:
+    bot = discord.Bot()
 
 
-@bot.slash_command()
-async def hello(ctx, name: str = None):
-    name = name or ctx.author.name
-    await ctx.respond(f"Hello {name}!")
+@bot.slash_command(name="ping", description="Test the latency of the bot")
+async def ping(ctx):
+    await ctx.respond(f"Pong! (Latency: {(bot.latency*1000):.2f} ms)")
 
 
 def run():
-    bot.run(getenv("DISCAPE_TOKEN"))
+    bot.run(TOKEN)
